77de4843ec21c1742361bf988404d39580fe2245
massina <121758831+Atinkene@users.noreply.github.com>
2025-03-22 07:20:13 +0000

    New

--- resources/views/elaab/composants/welcome.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="w-full mt-72">
-    <div class="flex justify-center items-center w-full mt-72">
-        <h1 class="text-blue-900 font-bold text-6xl"> Bienvenue sur E-Laab! </h1>
-    </div>
-    <div class="flex justify-center items-center text-blue-900">
-        <h3>votre plateforme d'évaluation en ligne</h3>
-    </div>
-</div>
